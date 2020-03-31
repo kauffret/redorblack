a17bc2a3c7487e6ab95761fd53b8bea28e399d73
kauffret <62516212+kauffret@users.noreply.github.com>
2020-03-31 22:05:04 +0200

    Modifiy deck.json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>Joueur</h3>
+    <ul v-for="person in persons" :key="person.id">
+      <li>{{ person.name }}</li>
+      <li>{{ person.status }}</li>
+      <li v-for="personCard in person.cards" :key="personCard.id">
+        <img width="15%" :src="getImgUrl(personCard.png)" v-bind:alt="personCard.number">
+      </li>
+    </ul>
     <h3>Deck complet</h3>
     <ul>
       <li v-for="item in deck" :key="item.id">{{ item.number }}{{ item.sign }}</li>
     </ul>
-    <button @click="picker">Pick a card!</button>
+    <button @click="pickACard">Pick a card!</button>
     <h3>Card</h3>
     <p>{{card.number}}{{card.sign}}</p>
     <img width="15%" :src="getImgUrl(card.png)" v-bind:alt="card.number">
@@ -22,14 +30,33 @@ export default {
   data() {
     return {
       deck: Deck,
-      card: ""
+      card: "",
+      persons: [
+        {
+          id: 0,
+          name: "Robert",
+          status: "Joue",
+          cards: []
+        },
+        {
+          id: 1,
+          name: "Kévin",
+          status: "En attente",
+          cards: []
+        }
+      ]
     };
   },
   methods: {
-    picker: function() {
+    pick: function() {
       var chosenNumber = Math.floor(Math.random() * this.deck.length);
       this.card = this.deck[chosenNumber];
+      console.log(this.persons);
       console.log(this.card);
+      return this.card;
+    },
+    pickACard: function() {
+      this.pick();
     },
     getImgUrl(imageName) {
       console.log(imageName);
